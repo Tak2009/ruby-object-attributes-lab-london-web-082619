d89401329b93939826e5e8a9d7202a68b8026aed
Takeshi Kimoto <lnuk2009jp@gmail.com>
2019-08-27 20:00:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,4 @@
 class Dog 
-
-  def initialize(breed)
-    @breed = breed
-  end
   
   def breed=(breed)
     @breed = breed
@@ -10,10 +6,6 @@ class Dog
   
   def breed 
     @breed
-  end
-  
-  def initialize(name)
-    @name = name
   end
   
   def name=(name)
